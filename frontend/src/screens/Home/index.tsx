e399486baf1f2588b7c4db3ea5b3538b2340d46f
saraazizhere <saraaziz180903@gmail.com>
2025-02-20 04:10:30 +0500

    Use jwt token

--- frontend/src/screens/Home/index.tsx
+++ frontend/src/screens/Home/index.tsx
@@ -1,8 +1,35 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
 import { MyAppBar } from "../../components";
+import { fetchData } from "../../api/childrenApi";
+import theme from "../../../theme";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetchData();
+        console.log(response);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getData();
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={styles.loader}>
+        <ActivityIndicator size="large" color={theme.colorSummerSky} />
+      </View>
+    );
+  }
+
   return (
     <View>
       <MyAppBar />
@@ -10,3 +37,11 @@ export default function Home() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  loader: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
