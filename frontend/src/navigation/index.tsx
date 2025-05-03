fa82099ba99446e926f14d56a4da485411772a99
saraazizhere <saraaziz180903@gmail.com>
2025-05-04 03:47:18 +0500

    Set session tracking

--- frontend/src/navigation/index.tsx
+++ frontend/src/navigation/index.tsx
@@ -6,6 +6,7 @@ import {
   ChildTest,
   ForgotPassword,
   GenderSelector,
+  Guardian,
   GuessExpression,
   Home,
   InputEmail,
@@ -25,6 +26,7 @@ const Stack = createStackNavigator<RootStackParamList>();
 export const Navigation = () => (
   <NavigationContainer>
     <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {/* Auth Flow */}
       <Stack.Screen name="Welcome" component={Welcome} />
       <Stack.Screen name="InputEmail" component={InputEmail} />
       <Stack.Screen name="InputPassword" component={InputPassword} />
@@ -32,12 +34,19 @@ export const Navigation = () => (
       <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
       <Stack.Screen name="VerificationCode" component={VerificationCode} />
       <Stack.Screen name="ResetPassword" component={ResetPassword} />
+
+      {/* Onboarding Flow */}
       <Stack.Screen name="ChildTest" component={ChildTest} />
       <Stack.Screen name="AgePicker" component={AgePicker} />
       <Stack.Screen name="GenderSelector" component={GenderSelector} />
       <Stack.Screen name="SpeechLevel" component={SpeechLevel} />
       <Stack.Screen name="SetGoal" component={SetGoal} />
+
+      {/* Main Flow */}
       <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Guardian" component={Guardian} />
+
+      {/* Games */}
       <Stack.Screen name="GuessExpression" component={GuessExpression} />
       <Stack.Screen name="MatchAndSort" component={MatchAndSort} />
       <Stack.Screen name="SocialScenario" component={SocialScenario} />
---
+++ frontend/src/screens/Main Screens/Settings/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Text } from "react-native";
+
+export default function Guardian() {
+  return <Text>Settings</Text>;
+}
